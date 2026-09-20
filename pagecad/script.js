88edf4fea6c81704b404df2a1701890b65f6c65d
arthursantos1805 <arthursantosgithub@gmail.com>
2026-09-19 22:06:39 -0300

    update no cadastro, sem mais usuarios repetidos

--- pagecad/script.js
+++ pagecad/script.js
@@ -10,6 +10,8 @@ form.addEventListener('submit', (evt)=> {
     evt.preventDefault();
 })
 
+let cadastroPerm = null
+
 const endpoint = 'https://apiusers-l8hp.onrender.com/users'
 
 //O Cadastro só vai funcionar se ligar a api, ent nn pode esquecer
@@ -20,32 +22,52 @@ btnCadastro.addEventListener('click', () => {
     const passw = inputSenha.value
     const passwconf = inputSenhaConf.value
 
-    if (email.includes('@gmail.com') || email.includes('@yahoo.com.br') || email.includes('@hotmail.com')) {
-        if (passw == passwconf) {
-            const userObj = {
-                user: user,
-                email: email,
-                passw: passw
+    fetch(endpoint)
+        .then(response => JSON.parse(response))
+        .then(data => {
+            for (usuario of data) {
+                if (usuario.user == user) {
+                    divErro.innerHTML = 'Usuário já cadastrado.'
+                    cadastroPerm = false
+                    break;
+                } else if (usuario.email == email) {
+                    divErro.innerHTML = 'Email já cadastrado.'
+                    cadastroPerm = false
+                    break;
+                } else {
+                    cadastroPerm = true
+                }
             }
+        })
+    
+    if (cadastroPerm) {
+        if (email.includes('@gmail.com') || email.includes('@yahoo.com.br') || email.includes('@hotmail.com')) {
+            if (passw == passwconf) {
+                const userObj = {
+                    user: user,
+                    email: email,
+                    passw: passw
+                }
 
-            fetch(endpoint, {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify(userObj)
-            })
-                .then(response => response.json())
-                .then(data => console.log(data))
-                .catch(() => {
-                    divErro.innerHTML = 'Fetch faiô'
+                fetch(endpoint, {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify(userObj)
                 })
+                    .then(response => response.json())
+                    .then(data => console.log(data))
+                    .catch(() => {
+                        divErro.innerHTML = 'Fetch faiô'
+                    })
 
-            divErro.innerHTML = 'Sucesso!'
+                divErro.innerHTML = 'Sucesso!'
+            } else {
+                divErro.innerHTML = 'As senhas não coincidem. Tente novamente.'
+            }
         } else {
-            divErro.innerHTML = 'As senhas não coincidem. Tente novamente.'
+            divErro.innerHTML = 'Formato de email inválido. Tente novamente.'
         }
-    } else {
-        divErro.innerHTML = 'Formato de email inválido. Tente novamente.'
     }
 })
