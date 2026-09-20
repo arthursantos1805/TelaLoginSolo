5d441b7678b9e7ac45bcf4e82cdd9041dddee4fb
arthursantos1805 <arthursantosgithub@gmail.com>
2026-09-19 21:04:20 -0300

    correção do window.location 2

--- pagecad/script.js
+++ pagecad/script.js
@@ -42,7 +42,7 @@ btnCadastro.addEventListener('click', () => {
                 })
 
             divErro.innerHTML = 'Sucesso!'
-            window.location = '../pagelogin/pagelogin.html'
+            window.location.replace('../pagelogin/pagelogin.html')
         } else {
             divErro.innerHTML = 'As senhas não coincidem. Tente novamente.'
         }
