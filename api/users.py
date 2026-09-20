1c897c99659fca067d337b589fc0dea1eb800a74
arthursantos1805 <arthursantosgithub@gmail.com>
2026-09-19 21:35:38 -0300

    correção no caminho do users.py

--- api/users.py
+++ api/users.py
@@ -16,19 +16,18 @@ app.add_middleware(
 dados = 0
 users = 0
 
-with open('./Exercicios/TelaLoginSolo/api/user.json', 'r') as file:
+with open('./user.json', 'r') as file:
     users = json.load(file)
 
 @app.get('/')
 def home():
     return 'Banco de Dados'
-
 @app.post('/users')
 def usersPost(user: dict):
     user['id'] = len(users)
     users.append(user)
 
-    with open('./Exercicios/TelaLoginSolo/api/user.json', 'w', encoding='utf-8') as file:
+    with open('./user.json', 'w', encoding='utf-8') as file:
         json.dump(users, file, indent=4, ensure_ascii=False)
 
     return {'Usuário': 'Cadastrado com Sucesso'}
